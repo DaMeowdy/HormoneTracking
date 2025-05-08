e423d4bad1262875ebf835b77ba0b799739e810d
Madison Sutton <YNotMadisonUwU@gmail.com>
2025-05-08 17:42:54 +1000

    starting coding because drawing 2 uml diagrams is secondary to developing a goated mobile application

---
+++ src/mobile_application/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Text } from 'react-native';
+
+import { Stack } from 'expo-router';
+import "./globals.css";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{headerShown:false}}/>
+    </Stack>
+  );
+}
